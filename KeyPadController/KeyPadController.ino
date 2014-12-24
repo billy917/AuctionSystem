0e7594b9de037d4b641699487e92eff0e7386bc6
Billy Wong <billy917@hotmail.com>
2014-12-23 22:34:41 -0500

    Updated KeyPadController with working code

--- KeyPadController/KeyPadController.ino
+++ KeyPadController/KeyPadController.ino
@@ -21,7 +21,7 @@ byte colPins[COLS] = {2, 3, 4}; //connect to the column pinouts of the keypad
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 Password password = Password( "9999" );
-
+char passChar[4] = {};
 int passwordLength = 0;
 
 void setup() {
@@ -32,10 +32,7 @@ void setup() {
   keypad.setDebounceTime(20);
   
   // try to print a number thats too long
-  matrix.print(0x0257,HEX);
-  //matrix.writeDigitNum(1,2);
-  //matrix.writeDigitNum(2,4);
-  //matrix.writeDigitNum(3,5);
+  matrix.print(0x0254,HEX);
   matrix.writeDisplay();
 }
 
@@ -47,7 +44,7 @@ void keypadEvent(KeypadEvent eKey){
     switch (eKey){
       case '*': checkPassword(); break;
       case '#': resetPassword(); break;
-      default: password.append(eKey); passwordLength++; updateLED(eKey);
+      default: password.append(eKey); passChar[passwordLength]=eKey; updateLED(); passwordLength++; 
     }
          
     if(4 == passwordLength){
@@ -56,23 +53,46 @@ void keypadEvent(KeypadEvent eKey){
   }
 }
 
-void pressedKeySound(){
+void updateLED(){
+  int length = passwordLength;
+  if(length == 4){
+   length = 3; 
+  }
   
+  matrix.clear();
+  int j=0;
+  for(int i=3-length;i<4;i++){
+    int k = i;
+    if(i >1){k++;}
+    matrix.writeDigitNum(k,passChar[j]-'0',false);
+    j++;
+  }
+  matrix.writeDisplay();
 }
 
-void updateLED(char e){
-  
+void blinkLED(int delayDuration, int times){
+  for(int i=0; i<times; i++){
+    matrix.clear();
+    matrix.writeDisplay();
+    delay(delayDuration);
+    updateLED();
+    delay(delayDuration);
+  }
 }
 
-void sucessUnlockSound(){
-  
+void resetLED(){
+/** Empties LED **/
+  matrix.clear();
+  matrix.writeDisplay();
 }
 
-void failUnlockSound(){
-  
-}
+/** SoundFX **/
+void pressedKeySound(){}
+void sucessUnlockSound(){}
+void failUnlockSound(){}
 
 void checkPassword(){
+  blinkLED(250,4);
   if (password.evaluate()){
     Serial.println("Success");
     //Add code to run if it works
@@ -87,6 +107,7 @@ void checkPassword(){
 void resetPassword(){
   password.reset();
   passwordLength = 0;
+  resetLED();
 }
 
 void loop() {
