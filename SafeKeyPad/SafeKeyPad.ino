582164c00b309aa3c63d619de3857f5b4a857a94
billy917@TweetyLaptop <billywong917@gmail.com>
2015-02-09 01:11:53 -0500

    Add safeKey pad prototype

---
+++ SafeKeyPad/SafeKeyPad.ino
@@ -0,0 +1,14 @@
+#include <Wire.h>
+#include "Constants.h"
+#include <Keypad.h>
+#include "SafeKeyPad.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
