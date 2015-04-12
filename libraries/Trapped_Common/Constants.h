c6e64d169b60c15f3ec28b8419a9035a1370c7f2
Billy Wong <billy917@hotmail.com>
2015-04-12 19:05:31 -0400

    update

--- libraries/Trapped_Common/Constants.h
+++ libraries/Trapped_Common/Constants.h
@@ -18,6 +18,7 @@ const int TRIP_LASER_I2C_ADDR = 111;
 const int SOUNDFX_I2C_ADDR = 113;
 const int CLOCK_I2C_ADDR = 114; //same as keypad on purpose
 const int PIANO_LOCK_I2C_ADDR = 115;
+const int WIRELESS_I2C_ADDR = 116;
 
 
 	// hardware I2C address
@@ -107,8 +108,9 @@ const int GLOBAL_LASER_ID[9] = {1,2,3,4,5,6,7,8,9};
 const int GLOBAL_LASER_MANAGER_ID[9] = {0,0,0,1,1,1,2,2,2};
 const int GLOBAL_SENSOR_ID[9] = {6,3,2,4,1,5,7,8,9};
 const int GLOBAL_SENSOR_MANAGER_ID[9] = {1,0,0,1,0,1,2,2,2};
+const bool GLOBAL_ENABLE_LASER[9] = {true,true,true,true,true,true,true,true,true}; // if laser is false, then sensor will not turn on
 const bool GLOBAL_ENABLE_SENSOR[9] = {true,true,true,true,true,true,true,true,true};
-const int SENSOR_TRESHOLD[9] = {30,40,30,40,30,40,30,30,40}; 
+const int SENSOR_TRESHOLD[9] = {30,40,30,40,30,20,10,30,40}; 
 	// 7,8,9 - 40,10,40
 const bool AUTO_CALIBRATE_SENSOR[9] = {false,false,false,false,false,false,false,false,false};
 /*
@@ -133,6 +135,12 @@ const uint8_t SENSOR_STATE_CANNOT_READ_DATA = 4;
 const uint8_t SENSOR_STATE_CANNOT_SET_INTERRUPT_CONTROL = 5;
 const uint8_t SENSOR_STATE_CANNOT_SET_THRESHOLD = 6;
 const uint8_t SENSOR_STATE_CANNOT_CLEAR_INTERRUPT = 7;
+
+const uint8_t GAME_STATE_RESTART = 1;
+const uint8_t GAME_STATE_RUNNING = 2;
+const uint8_t GAME_STATE_PAUSED = 3;
+const uint8_t GAME_STATE_STOPPED = 4;
+
 
 /*
 Clock Control message protocol
