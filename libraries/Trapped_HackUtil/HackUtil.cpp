f309b9f55ef720ac230fd92c2283cb8177bc4806
Akira Kanzaki <albertw1089@gmail.com>
2015-05-04 10:54:23 +0900

    (*)HackUtil -added Admin/Normal user screen

--- libraries/Trapped_HackUtil/HackUtil.cpp
+++ libraries/Trapped_HackUtil/HackUtil.cpp
@@ -42,6 +42,91 @@ void HackUtil::_displaySplashScreen(){
   GD.swap();
 }
 
+/* Assuming _user has already been defined beforehand */
+void HackUtil::_displayUserScreen(){
+    if (_user == ADMIN_USER){
+        _drawQuadSplit (0xff4b76, TAG_A1, 0xffaf4b, TAG_A2, 
+            0xf94bff, TAG_A3, 0x6a4bff, TAG_A4);
+        //TODO: _drawQuadText
+    }
+
+    else if (_user == NORMAL_USER){
+        //_drawQuadSplit (0xc0ff1b, 0xff881b, 0x1bff2d, 0x1c66ff);
+        _drawSplit (0xc0ff1b, TAG_N1, 0x1c66ff, TAG_N2);
+        //TODO: _drawQuadText
+    }
+
+    else{}
+
+}
+
+/* Assuming GD.get_inputs() has already been called beforehand */
+void HackUtil::_handleTouchInput(){
+    switch (GD.inputs.tag){
+        
+        case tagA1: break;
+        case tagA2: break;
+        case tagA3: break:
+        case tagA4: break;
+
+        case tagN1: break;
+        case tagN2: break;
+
+        
+        default: break;
+        
+    }
+}
+
+void HackUtil::_drawQuadSplit (int q1, int tagQ1, int q2, int tagQ2,
+int q3, int tagQ3, int q4, int tagQ4){
+    GD.Begin (RECTS);
+
+    /* Two opposite points make a rectangle */
+
+    /* Q1: Top-Left */
+    GD.ColorRGB (q1);
+    GD.Tag (tagQ1);
+    GD.Vertex2ii (0,0);
+    GD.Vertex2ii (239,135);
+
+    /* Q2: Top-Right */
+    GD.ColorRGB (q2);
+    GD.Tag (tagQ2);
+    GD.Vertex2ii (240,0);
+    GD.Vertex2ii (480,135);
+
+    /* Q3: Bottom-Left */
+    GD.ColorRGB (q3);
+    GD.Tag (tagQ3);
+    GD.Vertex2ii (0,136);
+    GD.Vertex2ii (239,272);
+
+    /* Q4: Bottom-Right */
+    GD.ColorRGB (q4);
+    GD.Tag (tagQ4);
+    GD.Vertex2ii (240,136);
+    GD.Vertex2ii (480,272);
+
+    GD.swap();
+
+}
+
+void HackUtil::_drawSplit (int q1, int tagQ1, int q2, int tagQ2){
+    /* Q1: Top */
+    GD.ColorRGB (q1);
+    GD.Tag (tagQ1);
+    GD.Vertex2ii (0,0);
+    GD.Vertex2ii (135, 480);
+
+    /* Q2: Bottom */
+    GD.ColorRGB (q2);
+    GD.Tag (tagQ2);
+    GD.Vertex2ii (136,0);
+    GD.Vertex2ii (272,480);
+
+}
+
 void HackUtil::run(){
 
 }
--- libraries/Trapped_HackUtil/HackUtil.h
+++ libraries/Trapped_HackUtil/HackUtil.h
@@ -6,10 +6,20 @@
 #ifndef HackUtil_h
 #define HackUtil_h
 
-#include "Arduino.h"  
+#include "Arduino.h"
 
 class HackUtil
 {
+  /* Admin screen tags */
+  #define TAG_A1 11
+  #define TAG_A2 12
+  #define TAG_A3 13
+  #define TAG_A4 14
+
+  /* Normal User screen tags */
+  #define TAG_N1 21
+  #define TAG_N2 22
+
   public:
     HackUtil();
     void init();
@@ -20,6 +30,18 @@ class HackUtil
     void _clearScreen();
     void _displaySplashScreen();
     void _displayMainScreen();
+
+    #define ADMIN_USER 0
+    #define NORMAL_USER 1
+    uint8_t _user;
+
+    void _displayUserScreen();
+    void _handleTouchInput();
+
+    /* Helper methods */
+    void _drawQuadSplit (int q1, int tagQ1, int q2, int tagQ2,
+        int q3, int tagQ3, int q4, int tagQ4);
+    void _drawSplit (int q1, int tagQ1, int q2, int tagQ2);
 
 };
 
